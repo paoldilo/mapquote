abbe93ce984cefadfef70750004bf0573cfd213d
paoldilo <paoldilo@yahoo.it>
2016-04-02 22:27:14 +0200

    4

--- mapquote/server.R
+++ mapquote/server.R
@@ -1,7 +1,17 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
 suppressPackageStartupMessages(library(quantmod))
 library(timeSeries)
 suppressPackageStartupMessages(library(googleVis))
-library(zoo)
+suppressPackageStartupMessages(library(zoo))
 options("getSymbols.warning4.0"=FALSE)
 #set dates
 startingDate <- as.Date("01/01/16",format="%m/%d/%y")
@@ -118,19 +128,29 @@ total <- merge(total,as.timeSeries(australia))
 # convert NA to 0
 total[is.na(total)]<-0
 
-#get only eon row and convert it to dataframe
-dataToPlot <- as.data.frame(total[53,])
-dataToPlot <- as.data.frame(t(dataToPlot))
-dataToPlot <- cbind(row.names(dataToPlot),dataToPlot)
-names(dataToPlot)<- c("Country","Value")
-dataToPlot$Country<- sub("\\."," ",dataToPlot$Country)
-dataToPlot$Perc <- paste0(sprintf("%.2f", round(dataToPlot$Value,2)),"%")
-#plot with google Vis
-G1a <- gvisGeoChart(dataToPlot, locationvar='Country', colorvar='Value',hovervar = 'Perc',
-                    options=list(projection="kavrayskiy-vii",
-                                 colorAxis="{values:[-4,0,4],
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+     #read the data from the fields
+     selected_date <- reactive({
+          input$date_select
+          selected_date
+     })
+     
+     
+  output$distPlot <- renderGvis({
+       #get only one row and convert it to dataframe
+       dataToPlot <- as.data.frame(total[53,])
+       dataToPlot <- as.data.frame(t(dataToPlot))
+       dataToPlot <- cbind(row.names(dataToPlot),dataToPlot)
+       names(dataToPlot)<- c("Country","Value")
+       dataToPlot$Country<- sub("\\."," ",dataToPlot$Country)
+       dataToPlot$Perc <- paste0(sprintf("%.2f", round(dataToPlot$Value,2)),"%")
+       #plot with google Vis
+        gvisGeoChart(dataToPlot, locationvar='Country', colorvar='Value',hovervar = 'Perc',
+                           options=list(projection="kavrayskiy-vii",
+                                        colorAxis="{values:[-4,0,4],
                                    colors:[\'red', \'white\', \'green']}",
-                                 backgroundColor= '#81d4fa',
-                                 datalessRegionColor= '#454545'))
-
-plot(G1a)
+                                        backgroundColor= '#81d4fa',
+                                        datalessRegionColor= '#454545'))
+  })
+})
---
+++ mapquote/ui.R
@@ -0,0 +1,21 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(rCharts)
+shinyUI(fluidPage(
+     titlePanel("Get stock indexes quotes on map"),
+     sidebarLayout(
+          sidebarPanel(
+             dateInput("date_select", "Label", value = Sys.Date()-1, min = NULL, max = NULL, format = "yyyy-mm-dd", startview = "month", language = "en")
+     ),
+     mainPanel(
+          htmlOutput("distPlot")
+     )
+)))
